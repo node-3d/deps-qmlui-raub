349e6c77f58bbfa8cd28b35839d7290035f94e56
Luis Blanco <raubtierxxx@gmail.com>
2017-08-30 21:46:13 +0300

    binary ok

--- include/qmlui.hpp
+++ include/qmlui.hpp
@@ -13,15 +13,17 @@ typedef void (*EventCb) (const char *data);
 
 // C-API:
 
-extern "C" QMLUI_DLLSPEC void qmlui_init(const char *cwdOwn, size_t wnd, size_t ctx, int w, int h, EventCb cb);
-extern "C" QMLUI_DLLSPEC void qmlui_resize(int w, int h);
-extern "C" QMLUI_DLLSPEC void qmlui_mouse(int type, int button, int buttons, int x, int y);
-extern "C" QMLUI_DLLSPEC void qmlui_keyboard(int type, int key, char text);
-extern "C" QMLUI_DLLSPEC void qmlui_use(const char *str, bool isFile);
-extern "C" QMLUI_DLLSPEC void qmlui_set(const char *obj, const char *prop, const char *json);
-extern "C" QMLUI_DLLSPEC void qmlui_get(const char *obj, const char *prop);
-extern "C" QMLUI_DLLSPEC void qmlui_invoke(const char *obj, const char *method, const char *json);
-extern "C" QMLUI_DLLSPEC void qmlui_libs(const char *libs);
+extern "C" QMLUI_DLLSPEC void qmlui_init(const char *cwdOwn, size_t wnd, size_t ctx);
+extern "C" QMLUI_DLLSPEC void qmlui_window(volatile int *i, int w, int h, EventCb cb);
+extern "C" QMLUI_DLLSPEC void qmlui_close(int i);
+extern "C" QMLUI_DLLSPEC void qmlui_resize(int i, int w, int h);
+extern "C" QMLUI_DLLSPEC void qmlui_mouse(int i, int type, int button, int buttons, int x, int y);
+extern "C" QMLUI_DLLSPEC void qmlui_keyboard(int i, int type, int key, char text);
+extern "C" QMLUI_DLLSPEC void qmlui_use(int i, const char *str, bool isFile);
+extern "C" QMLUI_DLLSPEC void qmlui_set(int i, const char *obj, const char *prop, const char *json);
+extern "C" QMLUI_DLLSPEC void qmlui_get(int i, const char *obj, const char *prop);
+extern "C" QMLUI_DLLSPEC void qmlui_invoke(int i, const char *obj, const char *method, const char *json);
+extern "C" QMLUI_DLLSPEC void qmlui_libs(int i, const char *libs);
 extern "C" QMLUI_DLLSPEC void qmlui_plugins(const char *plugins);
 
 #endif // QMLUI_HPP
