c55d0570ae612682e1c899f340836f8cc627297d
Luis Blanco <luisblanco1337@gmail.com>
2019-02-08 22:01:55 +0300

    Fix osx build 4

--- qt/platform.hpp
+++ qt/platform.hpp
@@ -31,7 +31,7 @@
 	typedef NSOpenGLContext CtxHandle;
 	typedef int WndHandle;
 	
-	#include <QtPlatformHeaders/QCocoaNativeContext>
+	#include <QCocoaNativeContext>
 	typedef QCocoaNativeContext NativeContext;
 	
 #endif
